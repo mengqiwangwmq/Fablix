3b6cd8e97a1f9712d8caeccfd8c953b1fdf03a69
mengqiwangwmq <mengqw3@uci.edu>
2018-06-11 16:46:15 -0700

    script

--- script.py
+++ script.py
@@ -3,9 +3,9 @@ import sys
 
 if len(sys.argv) < 2:
     print("Please input the file name.")
-
-f = open(sys.argv[1])
-nums = csv.reader(f)
-nums = list(map(lambda x: int(x), nums))
-avg = sum(nums) / len(nums)
-print("The average number in " + sys.argv[1] + " is: " + str(avg))
+else:
+    f = open(sys.argv[1])
+    nums = csv.reader(f)
+    nums = list(map(lambda x: int(x[0]), nums))
+    avg = sum(nums) / len(nums)
+    print("The average number in " + sys.argv[1] + " is: " + str(avg))
